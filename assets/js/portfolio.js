ad67a318f9c27fec0b3dfede6bda50856eea8944
joshua-unseen <corpselight@gmail.com>
2019-04-19 20:12:40 -0700

    Page specific script files.

---
+++ assets/js/portfolio.js
@@ -0,0 +1,12 @@
+var portfolio = {
+    works: [
+        {
+            name: "Hangman",
+            repoName: "",
+        },
+        {
+            name: "Star Wars Combat",
+            repoName: "",
+        },
+    ],
+}
